5cb2f8ddaefd2725841482363f6d748460a1467a
Zaid Al Lahham <zlahham@gmail.com>
2015-10-05 16:05:47 +0100

    added a post controller

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ function($stateProvider, $urlRouterProvider) {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
     });
 
   $urlRouterProvider.otherwise('home');
@@ -22,9 +27,7 @@ function($stateProvider, $urlRouterProvider) {
   return o;
 }])
 
-.controller('MainCtrl', [
-'$scope', 'posts',
-function($scope, posts){
+.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
   $scope.test = 'Hello world!';
 
   $scope.posts = posts.posts;
@@ -34,7 +37,11 @@ function($scope, posts){
     $scope.posts.push({
       title: $scope.title,
       link: $scope.link,
-      upvotes: 0
+      upvotes: 0,
+      comments: [
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+      ]
     });
     $scope.title = '';
     $scope.link = '';
@@ -43,4 +50,8 @@ function($scope, posts){
   $scope.incrementUpvotes = function(post) {
     post.upvotes += 1;
   };
+}])
+
+.controller('PostsCtrl', [ '$scope', '$stateParams', 'posts', function($scope, $stateParams, posts){
+  $scope.post = posts.posts[$stateParams.id];
 }]);
